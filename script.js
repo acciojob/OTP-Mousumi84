03d14e73ee510c00ea45123caba896e58469d399
Mousumi Dsa <58068282+Mousumi84@users.noreply.github.com>
2024-02-24 21:00:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-//your JS code here. If required.
+let container=document.querySelector(".code-container");
+let lastValue=null
+
+for(let i=0;i<=5;i++)
+{
+let cell=container.children[i];
+cell.addEventListener("input", inputFocus);    
+cell.addEventListener("keyup", onkey);
+}
+
+function inputFocus(eventObject) {
+   
+    const targetCell=eventObject.target;
+
+    if(targetCell.value)
+    {
+        lastValue=targetCell;
+        targetCell.nextElementSibling && targetCell.nextElementSibling.focus();
+    } 
+}
+
+function onBackspace(eveObj) {
+    let currentCell=eveObj.target;
+    currentCell.value="";
+    currentCell.previousElementSibling && currentCell.previousElementSibling.focus();
+}
+ 
+function onEnter(eveObj) {
+    let currentCell=eveObj.target;
+    if(currentCell.value)
+    {
+        currentCell.nextElementSibling && currentCell.nextElementSibling.focus();
+    }
+}
+
+function onkey(eKey) {
+    if(eKey.key === "Backspace")
+    {
+        onBackspace(eKey);
+    }
+    else if(eKey.key === "Enter")
+    {
+        onEnter(eKey);
+    }
+}
